1e7ecc72db3b517dcae192ea225b06d605b5283b
charliechen <charliechen@lionfin.com>
2021-10-28 19:35:44 +0800

    add comments explaining basic concepts

--- rust/03_minigrep/src/lib.rs
+++ rust/03_minigrep/src/lib.rs
@@ -8,14 +8,18 @@ pub struct Config {
     pub filename: String
 }
 
+// define a custom error for Box<dyn Error>
 #[derive(Debug, Clone)]
+// field-less struct: tuple struct
 struct NotFoundError(String);
 
 impl fmt::Display for NotFoundError {
+    // formatter decides how to display the content
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "can not find the query: {}", self.0)
     }
 }
+// the implementation of customer error can be empty
 impl Error for NotFoundError {}
 
 impl Config {
@@ -31,6 +35,7 @@ impl Config {
 }
 
 pub fn run(cfg: Config) -> Result<(), Box<dyn Error>> {
+    // error propagation
     let content = fs::read_to_string(&cfg.filename)?;
     println!("The query is: {}", cfg.query);
     println!("The filename is: {}", cfg.filename);
@@ -40,5 +45,6 @@ pub fn run(cfg: Config) -> Result<(), Box<dyn Error>> {
             return Ok(())
         }
     }
+    // initial the Box smart pointer using `::new`
     Err(Box::new(NotFoundError(cfg.query)))
 }
