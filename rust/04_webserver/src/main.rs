74be8d81aaf9275a71993520563606e4f666bc90
charliechen <charliechen@lionfin.com>
2022-09-30 14:11:20 +0800

    rust: add /sleep to webserver to see how single thread blocks

--- rust/04_webserver/src/main.rs
+++ rust/04_webserver/src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn handle_connection(mut stream: TcpStream) {
@@ -16,10 +18,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     // destructuring assignment
     // like in typescript
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 400 Not Found", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(10));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 400 Not Found", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
